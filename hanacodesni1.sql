3ea7a7419f8ff2c5928383f0a5519e9a8b5192a6
zbourn <zbourn@users.noreply.github.com>
2015-06-02 03:31:39 +0530

    Update hanacodesni1.sql

--- hanacodesni1.sql
+++ hanacodesni1.sql
@@ -6,3 +6,18 @@ select current_user from dummy;
 select * from "SYS"."AFL_FUNCTIONS" where package_name = 'PAL';
 --Set Application schema
 set schema "NEO_4FH1WG5QDRJVNWPCITP7OUI
+
+--Create a Table
+--if you want mixed case of table and column name put them in ""
+create column table "Tweets"(
+"id" varchar(256) not null,
+"created" timestamp,
+"text" nvarchar(256),
+"lang" varchar(256),
+"user" varchar(256),
+"replyUser" varchar(256),
+"retweetedUser" varchar(256),
+"lat" double,
+"lon" double,
+primary key ("id")
+);
