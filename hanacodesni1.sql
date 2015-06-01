95e2be58dd4495343652692d6ca2f462847dc672
zbourn <zbourn@users.noreply.github.com>
2015-06-02 02:02:54 +0530

    Create hanacodesni1.sql

---
+++ hanacodesni1.sql
@@ -0,0 +1,8 @@
+--Identify Schema name
+select SCHEMA_NAME from "HCP"."HCP_DEV_METADATA";
+--Identify Current user name
+select current_user from dummy;
+--Varify if predictive analysis library is available
+select * from "SYS"."AFL_FUNCTIONS" where package_name = 'PAL';
+--Set Application schema
+set schema "NEO_4FH1WG5QDRJVNWPCITP7OUI
